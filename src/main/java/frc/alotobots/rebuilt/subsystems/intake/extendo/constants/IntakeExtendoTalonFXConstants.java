a7151a4fe9b94580d8134300a2d3f798b7e5113d
Sean Ernstes <sean.ernstes@proton.me>
2026-03-05 21:05:25 -0500

    Update at 'Thu Mar 05 21:05:25 EST 2026'

--- src/main/java/frc/alotobots/rebuilt/subsystems/intake/extendo/constants/IntakeExtendoTalonFXConstants.java
+++ src/main/java/frc/alotobots/rebuilt/subsystems/intake/extendo/constants/IntakeExtendoTalonFXConstants.java
@@ -47,7 +47,7 @@ public class IntakeExtendoTalonFXConstants {
     /** TalonFX-specific PID and motion control constants for Position mode (Position mode). */
     public static final class PositionPIDConstants {
       /** Position control proportional gain */
-      public static final double KP = 5;
+      public static final double KP = 1;
 
       /** Position control integral gain */
       public static final double KI = 0.0;
