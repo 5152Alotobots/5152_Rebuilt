543c436c3ac0afe20ef36eb4f05e3372266de726
Sean Ernstes <sean.ernstes@proton.me>
2026-03-05 21:35:18 -0500

    Update at 'Thu Mar 05 21:35:18 EST 2026'

--- src/main/java/frc/alotobots/rebuilt/subsystems/launcher/turret/constants/TurretTalonFXSConstants.java
+++ src/main/java/frc/alotobots/rebuilt/subsystems/launcher/turret/constants/TurretTalonFXSConstants.java
@@ -51,7 +51,7 @@ public class TurretTalonFXSConstants {
       public static final double TURRET_VELOCITY_KS = 0.36;
 
       /** Velocity feedforward gain */
-      public static final double TURRET_VELOCITY_KV = 0.65;
+      public static final double TURRET_VELOCITY_KV = 1.25;
     }
   }
 
