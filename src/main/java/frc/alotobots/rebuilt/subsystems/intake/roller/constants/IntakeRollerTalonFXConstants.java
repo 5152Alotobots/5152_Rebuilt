6ac0e5046f57a4606140aed959272c09f3041bcf
alanmeye <alotobots5152@gmail.com>
2026-03-06 16:57:59 -0500

    Update at 'Fri Mar 06 16:57:59 EST 2026'

--- src/main/java/frc/alotobots/rebuilt/subsystems/intake/roller/constants/IntakeRollerTalonFXConstants.java
+++ src/main/java/frc/alotobots/rebuilt/subsystems/intake/roller/constants/IntakeRollerTalonFXConstants.java
@@ -37,9 +37,9 @@ public class IntakeRollerTalonFXConstants {
   public static final class MotorSafetyLimits {
     public static final Current TORQUE_FORWARD_AMP_LIMIT = Amps.of(20);
     public static final Current TORQUE_REVERSE_AMP_LIMIT = Amps.of(-20);
-    public static final Current STATOR_AMP_LIMIT = Amps.of(20);
-    public static final Current SUPPLY_PEAK_LIMIT = Amps.of(25);
-    public static final Current SUPPLY_SUSTAINED_LIMIT = Amps.of(20);
+    public static final Current STATOR_AMP_LIMIT = Amps.of(30);
+    public static final Current SUPPLY_PEAK_LIMIT = Amps.of(35);
+    public static final Current SUPPLY_SUSTAINED_LIMIT = Amps.of(25);
     public static final Time SUPPLY_PEAK_DURATION = Seconds.of(0.25);
   }
 
