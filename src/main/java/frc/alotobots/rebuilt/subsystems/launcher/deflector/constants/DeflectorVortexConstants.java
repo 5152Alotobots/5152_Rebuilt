d85b454dca81f933f5e8ca50f6fd74d462040234
alanmeye <alotobots5152@gmail.com>
2026-03-06 19:09:58 -0500

    Update at 'Fri Mar 06 19:09:58 EST 2026'

--- src/main/java/frc/alotobots/rebuilt/subsystems/launcher/deflector/constants/DeflectorVortexConstants.java
+++ src/main/java/frc/alotobots/rebuilt/subsystems/launcher/deflector/constants/DeflectorVortexConstants.java
@@ -78,7 +78,7 @@ public class DeflectorVortexConstants {
   public static final SensorDirectionValue DEFLECTOR_ENCODER_SENSOR_DIRECTION =
       SensorDirectionValue.Clockwise_Positive;
 
-  public static final double DEFLECTOR_ENCODER_MAGNET_OFFSET = 0.394287109375;
+  public static final double DEFLECTOR_ENCODER_MAGNET_OFFSET = 0.3759765625;
   public static final double DEFLECTOR_ABSOLUTE_SENSOR_DISCONTINUITY_POINT = 0.5;
 
   /**
