e63bdd7d1344ea8fe9f49ba4cd21ce4d27b74bbe
rkortlandt <rowankortlandt@otsegops.org>
2026-03-21 11:19:31 -0400

    default for roller

---
+++ src/main/java/frc/alotobots/rebuilt/subsystems/roller/commands/DefaultRollerRunOpenLoop.java
@@ -0,0 +1,50 @@
+/*
+* ALOTOBOTS - FRC Team 5152
+  https://github.com/5152Alotobots
+* Copyright (C) 2026 ALOTOBOTS
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Source code must be publicly available on GitHub or an alternative web accessible site
+*/
+package frc.alotobots.rebuilt.subsystems.roller.commands;
+
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.alotobots.rebuilt.subsystems.roller.RollerSubsystem;
+
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+public class DefaultRollerRunOpenLoop extends Command {
+  private final RollerSubsystem rollerSubsystem;
+  private final DoubleSupplier inputSupplier;
+
+  public DefaultRollerRunOpenLoop(
+      RollerSubsystem rollerSubsystem, DoubleSupplier inputSupplier) {
+    this.rollerSubsystem = rollerSubsystem;
+    this.inputSupplier = inputSupplier;
+    addRequirements(rollerSubsystem);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    rollerSubsystem.runRollerPercentOutput(inputSupplier.getAsDouble());
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    rollerSubsystem.stop();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
