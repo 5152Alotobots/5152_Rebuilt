2f0f353cd398391cdd51c1026d3496593ac478ea
alanmeye <alotobots5152@gmail.com>
2026-04-16 14:00:18 -0400

    Update at 'Thu Apr 16 14:00:17 EDT 2026'

--- src/main/java/frc/alotobots/rebuilt/subsystems/launcher/turret/constants/TurretConstants.java
+++ src/main/java/frc/alotobots/rebuilt/subsystems/launcher/turret/constants/TurretConstants.java
@@ -36,10 +36,10 @@ public class TurretConstants {
   /** Contains physical limits and safety thresholds for the turret. */
   public static final class Limits {
     /** Maximum allowed angle */
-    public static final Angle TURRET_MAX_ANGLE = Degrees.of(215);
+    public static final Angle TURRET_MAX_ANGLE = Degrees.of(225);
 
     /** Minimum allowed angle */
-    public static final Angle TURRET_MIN_ANGLE = Degrees.of(-125);
+    public static final Angle TURRET_MIN_ANGLE = Degrees.of(-160);
 
     /** Maximum open loop percent output */
     public static final double TURRET_MAX_OPEN_LOOP_PERCENTAGE = 0.2;
