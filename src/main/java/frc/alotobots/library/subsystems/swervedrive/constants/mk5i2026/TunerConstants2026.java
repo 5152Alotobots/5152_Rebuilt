87895eff8c5bd04a6896e076740eb1a274f02f3b
alanmeye <alotobots5152@gmail.com>
2026-04-17 16:28:19 -0400

    Update at 'Fri Apr 17 16:28:18 EDT 2026'

--- src/main/java/frc/alotobots/library/subsystems/swervedrive/constants/mk5i2026/TunerConstants2026.java
+++ src/main/java/frc/alotobots/library/subsystems/swervedrive/constants/mk5i2026/TunerConstants2026.java
@@ -288,8 +288,8 @@ public class TunerConstants2026 implements TunerConstants {
   public static class CustomConstants {
     // Custom constants go here
     // Pathfinding PID constants
-    public static final PIDConstants translationPid = new PIDConstants(2.5, 0, 0.04);
-    public static final PIDConstants rotationPid = new PIDConstants(3.0, 0, 0.0);
+    public static final PIDConstants translationPid = new PIDConstants(3.5, 0, 0.04);
+    public static final PIDConstants rotationPid = new PIDConstants(4.2, 0, 0.0);
     public static final PPHolonomicDriveController PP_HOLONOMIC_DRIVE_CONTROLLER =
         new PPHolonomicDriveController(translationPid, rotationPid);
     public static final PathConstraints PATHFINDING_CONSTRAINTS =
